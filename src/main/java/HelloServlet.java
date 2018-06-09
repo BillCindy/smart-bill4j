c7526c22624de256f557a0c92696ac00a8a0e68b
bill-Smith <billcindy6688@gmail.com>
2018-06-09 10:35:37 +0800

    first commit
    Signed-off-by: [liuwenbin] <[liuwbt@yonyou.com]>

--- src/main/java/HelloServlet.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by bill on 2018/6/9.
- */
-public class HelloServlet {
-}
---
+++ src/main/java/org/dynamic/bill4j/HelloServlet.java
@@ -0,0 +1,26 @@
+package org.dynamic.bill4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by bill on 2018/6/9.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+//        req.setAttribute("author","bill smith");
+        req.getRequestDispatcher("/WEB-INF/jsp/hello2.jsp").forward(req, resp);
+    }
+}
