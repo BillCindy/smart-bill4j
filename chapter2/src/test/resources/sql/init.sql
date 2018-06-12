af8456b6a989e8c3092d5a71f60d56e3dd83327a
bill <billcindy6688@gmail.com>
2018-06-13 07:13:58 +0800

    test commit at home

--- chapter2/src/test/resources/sql/init.sql
+++ chapter2/src/test/resources/sql/init.sql
@@ -9,4 +9,5 @@ CREATE TABLE customer (
 ) engine = InnoDB DEFAULT charset=utf8;
 
 INSERT INTO customer VALUES (1,'customer1','bill','18232480330','1823234994@163.com',null);
-INSERT INTO customer VALUES (2,'customer2','cindy','18232480333','1823234993@163.com',null)
+INSERT INTO customer VALUES (2,'customer2','cindy','18232480333','1823234993@163.com',null);
+INSERT INTO customer VALUES (3,'customer3','cindy','18232480553','1823234993@123.com',null)
